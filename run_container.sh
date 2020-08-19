ec4be1972fee1d311dc57c2605c673941b3de79e
Griffin Knipe <knipegp@gmail.com>
2020-08-19 14:40:24 -0400

    Make this a script repo

--- run_container.sh
+++ run_container.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
 
-# From: https://sipb.mit.edu/doc/safe-shell/
 set -euf -o pipefail
 
-vol_path=""
+run_container(){
+    if [ "$1" == "-h" ]; then
+        echo "Usage: run_container.sh [-h] image_name [--volume-path path]"
+        exit 0
+    fi
 
-if [ "$1" == "-h" ]; then
-	echo "Usage: run_container.sh [-h] image_name [--volume-path path]"
-	exit 0
-fi
+    if [ $# -eq 0 ]; then
+        echo "Docker image name not supplied"
+        exit 1
+    fi
+    local vol_path=""
+    local newuser=dev
+    local run_args="-it --rm -w /mnt -u 0"
+    local image_name="$1"
+    shift
+    while test $# -gt 0; do
+        case "$1" in
+            --volume-path)
+                shift
+                local vol_path=$1
+                shift
+                ;;
+            *)
+                run_args="$run_args $1"
+                shift
+                ;;
+        esac
+    done
+    local groupid=""
+    groupid="$(id -g)"
+    local usercommand
+    usercommand="\"groupadd -o -g $groupid $newuser && useradd -m -o -u $(id -u) -g $groupid $newuser && su $newuser\""
+    if [ -n "$vol_path" ]; then
+        run_args="$run_args --mount type=bind,source=$vol_path,target=/mnt"
+    fi
+    eval "docker run $run_args $image_name /bin/bash -c $usercommand"
+}
 
-if [ $# -eq 0 ]; then
-	echo "Docker image name not supplied"
-	exit 1
-fi
-image_name="$1"
-shift
-
-run_args=""
-while test $# -gt 0; do
-	case "$1" in
-		--volume-path)
-			shift
-			vol_path=$1
-			shift
-			;;
-        *)
-            run_args="$run_args $1"
-            shift
-            ;;
-	esac
-done
-
-USER_ID="$(id -u)"
-
-HOME_DIR="/home/developer"
-
-mount_cmd=""
-if [ ! -z "$vol_path" ]; then
-	mount_cmd="--mount type=bind,source=$vol_path,target=/mnt"
-fi
-
-
-docker_cmd="docker run --net=host -it --rm $run_args --user=$USER_ID -w $HOME_DIR $mount_cmd $image_name /bin/bash"
-# echo "$docker_cmd"
-eval "$docker_cmd"
+run_container "$@"
